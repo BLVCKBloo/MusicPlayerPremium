53de39bf869dacdaa91cb040a76f169a46ef8d5b
BLVCKBloom <103446053+BLVCKBloo@users.noreply.github.com>
2023-03-10 21:55:22 +0700

    Update key.js

--- musicPlayerPremium/scp/key.js
+++ musicPlayerPremium/scp/key.js
@@ -1,6 +1,6 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
+document.body.style.backgroundImage = "url("./musicPlayerPremium/imgs/imgLogo/Neon-Lo-Fi-Background-HD.jpg")";
 const heading = $("header h2"),
   headingSinger = $("header h5"),
   cdThumb = $(".cd-right"),
