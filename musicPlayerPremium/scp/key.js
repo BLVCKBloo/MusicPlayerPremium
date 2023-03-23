de42d54779144af3e537972c963c341fa8e55bea
BLVCKBloom <103446053+BLVCKBloo@users.noreply.github.com>
2023-03-23 21:11:50 +0700

    Update key.js

--- musicPlayerPremium/scp/key.js
+++ musicPlayerPremium/scp/key.js
@@ -467,6 +467,10 @@ const app = {
     };
 
     // ------------------------------=-=-=-=-=-=-=-=
+    progress.onclick = function (e) {
+      const seekTime = (audio.duration / 100) * e.target.value;
+      audio.currentTime = seekTime;
+    };
     audio.ontimeupdate = function () {
       if (audio.duration) {
         const currentProgress = Math.floor(
@@ -504,10 +508,6 @@ const app = {
       timeDuration.innerText = `${
         isNaN(duration_minutes) ? "00" : duration_minutes
       }:${isNaN(duration_seconds) ? "00" : duration_seconds}`;
-    };
-    progress.onchange = function (e) {
-      const seekTime = (audio.duration / 100) * e.target.value;
-      audio.currentTime = seekTime;
     };
     volumeBar.oninput = function (e) {
       const seekVolume = e.target.value / 100;
@@ -639,9 +639,9 @@ const app = {
       _this.isAutoNextSong = !_this.isAutoNextSong;
       autoNextSongBtn.classList.toggle("active", _this.isAutoNextSong);
       getNextSongSpan = $(".autonext-span");
-      _this.isAutoNextSong
-        ? (getNextSongSpan.innerText = "Tự động chuyển bài: Bật")
-        : (getNextSongSpan.innerText = "Tự động chuyển bài: Tắt");
+      // _this.isAutoNextSong
+      //   ? (getNextSongSpan.innerText = "Tự động chuyển bài: Bật")
+      //   : (getNextSongSpan.innerText = "Tự động chuyển bài: Tắt");
       _this.render();
     };
 
@@ -656,6 +656,55 @@ const app = {
         optionControl(e);
       };
     });
+    dragElement(listBtn);
+
+    function dragElement(elmnt) {
+      const getParentElement = elmnt.parentElement;
+
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+      if (document.getElementById(getParentElement.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(getParentElement.id + "header").onmousedown =
+          dragMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        getParentElement.onmousedown = dragMouseDown;
+      }
+
+      function dragMouseDown(e) {
+        elmnt.querySelector(".list-btn-span").classList.add("disable");
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        getParentElement.style.top = getParentElement.offsetTop - pos2 + "px";
+        getParentElement.style.left = getParentElement.offsetLeft - pos1 + "px";
+      }
+
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
     // ------------------------------
     optionControl = function (e) {
       deleteThisSong = function (lists, btn, index, list2) {
